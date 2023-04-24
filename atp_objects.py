206e90fd10ef08c6586e0a24698d89aa332f4510
Carlos Daniel Jiménez <cd.jimenez99@gmail.com>
2023-04-24 11:44:46 -0500

    create_xml_element en clase Resistor Se creó el método y sus tests básicos.

--- atp_objects.py
+++ atp_objects.py
@@ -16,12 +16,57 @@ class Resistor:
     
     
     def __init__(self, resistance, x_pos, y_pos):
-        self.resistance = resistance
-        self.x_pos = x_pos
-        self.y_pos = y_pos
+        self.resistance = str(resistance)
+        self.x_pos = str(x_pos)
+        self.y_pos = str(y_pos)
     
     def create_xml_element(self):
         '''
-        
+        Genera el objeto ElementTree base de la resistencia.
+
+        Return:
+            (xml.etree.ElementTree.Element): Objeto ElementTree.
         '''
-    
+
+        comp = ET.Element(  'comp',
+                            attrib={'Name': 'RESISTOR'},
+                        )
+
+        comp_content = ET.SubElement(   comp,
+                                        'comp_content',
+                                        attrib={
+                                                'PosX': self.x_pos,
+                                                'PosY': self.y_pos,
+                                                'Icon': "default",
+                                                },
+                                    )
+
+        ET.SubElement(  comp_content, 'node',
+                        attrib={
+                                'Name': 'From',
+                                'Value': '',
+                                'Kind': '1',
+                                'PosX': '-20',
+                                'PosY': '0',
+                                },
+                    )
+
+        ET.SubElement(  comp_content, 'node',
+                        attrib={
+                                'Name': 'To',
+                                'Value': '',
+                                'Kind': '1',
+                                'PosX': '20',
+                                'PosY': '0',
+                                },
+                    )
+
+        ET.SubElement(  comp_content, 'data',
+                        attrib={
+                                'Name': 'R',
+                                'Value': self.resistance,
+                                'Kind': '1',
+                                },
+                    )
+
+        return comp
--- tests/test_atp_objects.py
+++ tests/test_atp_objects.py
@@ -1,17 +1,47 @@
 import unittest
+import xml.etree.ElementTree as ET
+import xml.dom.minidom as minidom
+
+import os
+import sys
+sys.path.append(os.getcwd())
+
+from atp_objects import Resistor
 
 
-class TestMathUtils(unittest.TestCase):
-    
-    def test_Resistor_create_xml_element(self):
-        expected = '''
-        <comp Name="RESISTOR">
-        <comp_content PosX="4710" PosY="4940" Icon="default">
-            <node Name="From" Value="" Kind="1" PosX="-20" PosY="0" NamePosX="-4" NamePosY="0"/>
-            <node Name="To" Value="" Kind="1" PosX="20" PosY="0" NamePosX="4" NamePosY="0"/>
-            <data Name="R" Value="1000"/>
-        </comp_content>
-        </comp>
+class Test_ATPObjects(unittest.TestCase):
+
+    def setUp(self):
+        self.resistor = Resistor(
+                            resistance = 1000,
+                            x_pos = 4710,
+                            y_pos = 4940,
+                            )
+
+
+    def test_Resistor_int_resistance(self):
+        '''
+        Testear que el valor de la resistencia se asignó adecuadamente.
         '''
 
+        self.xml_element = self.resistor.create_xml_element()
+        data_element = self.xml_element.find('./comp_content/data')
+        self.assertEqual(data_element.get('Value'), '1000')
+
+    def test_Resistor_xml_element_has_correct_estructure(self):
+        '''
+        Chequea si el elemnto XML tiene dos elementos nodes y uno data.
+        '''
         
+        self.xml_element = self.resistor.create_xml_element()
+        self.assertIsNotNone(self.xml_element)
+        self.assertEqual(self.xml_element.tag, 'comp')
+
+        node_elements = self.xml_element.findall('./comp_content/node')
+        data_elements = self.xml_element.findall('./comp_content/data')
+        self.assertEqual(len(node_elements), 2)
+        self.assertEqual(len(data_elements), 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
